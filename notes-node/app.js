538b91df99ed40bf3a03d85c04f7addade93053c
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-13 21:38:46 -0800

    working with modules and module.export object

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,8 +1,10 @@
-console.log('Starting App.');
+console.log('Starting App.js');
 
 const fs = require('fs');
 const os = require('os');
-
-fs.writeFileSync('greetings.txt', 'Hello World!');
-let username = os.userInfo().username;
-console.log('Hello ' + username)
+const notes = require('./notes');
+const add = notes.addNotes(5, -8);
+console.log(add)
+// fs.writeFileSync('greetings.txt', 'Hello World!');
+// let username = os.userInfo().username;
+// console.log('Hello ' + username + ', you are ' + notes.age + ' years old.')
---
+++ notes-node/notes.js
@@ -0,0 +1,6 @@
+console.log('Starting notes.js');
+
+
+module.exports.addNotes = (a, b) => {
+	return a + b;
+}
