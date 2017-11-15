8eb68c25894264a88fb023135b6b0f3028ac7306
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-14 20:49:28 -0800

    checking lodash module

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,9 +2,12 @@ console.log('Starting App.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
-const add = notes.addNotes(5, -8);
-console.log(add)
-// fs.writeFileSync('greetings.txt', 'Hello World!');
-// let username = os.userInfo().username;
-// console.log('Hello ' + username + ', you are ' + notes.age + ' years old.')
+
+
+// console.log(_.isString(true))
+// console.log(_.isString('String'))
+
+let filteredArray = _.uniq(['Ahmed', 1, 'Ahmed', 1,2,3]);
+console.log(filteredArray)
