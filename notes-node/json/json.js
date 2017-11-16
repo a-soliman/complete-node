1327cfef0437a05bb514a1f1df7c34c565070705
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-15 20:05:50 -0800

    more with json methods and fs module

--- notes-node/json/json.js
+++ notes-node/json/json.js
@@ -1,9 +1,24 @@
-let obj = {
-	name: 'Andrew'
+// let obj = {
+// 	name: 'Andrew'
+// };
+
+// let stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+// let backToObj = JSON.parse(stringObj);
+// console.log(typeof backToObj)
+
+const fs = require('fs');
+
+let originalNote = {
+	title: 'sometitle',
+	body: 'some body'
 };
 
-let stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
-let backToObj = JSON.parse(stringObj);
-console.log(typeof backToObj)
+let originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.txt', originalNoteString);
+
+
+let noteString = fs.readFileSync('notes.txt', 'utf-8');
+let noteObj = JSON.parse(noteString)
+console.log(noteObj)
