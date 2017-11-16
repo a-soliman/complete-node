79b0b7d9f748e3a278813af46e851feb7f820e86
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-15 20:43:12 -0800

    coded the add note function, checked for uniqness

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,20 +1,38 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+
 let notes = [];
 
 let addNote = (title, body) => {
-	console.log('Adding Note. ' + title + body);
+	let notes = [];
+	let note = { 
+		title, 
+		body
+	};
 
-	let newNote = {title: title, body: body};
-	notes.push(newNote);
+	try {
+		let notesString = fs.readFileSync('notes-data.json');
+		notes = JSON.parse(notesString);
+	} catch(e) {
+
+	}
+	
+	let duplicateNotes = notes.filter((note) => note.title === title);
+
+	if(duplicateNotes.length === 0 ) {
+		
+		notes.push(note);
+		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+	}
+
+	
+
 };
 
 let listNotes = () => {
 	console.log('Listing Notes...');
-
-	for ( let i = 0; i < notes.length; i++ ) {
-		console.log(`TITLE : ${note[i].title} BODY: ${note[i].body}.`);
-	} 
 };
 
 let removeNote = (title) => {
@@ -23,6 +41,18 @@ let removeNote = (title) => {
 
 let readNote = (title) => {
 	console.log('Reading a note...')
+};
+
+let isUniqeNode = (title, notesArr) => {
+
+	for (let i = 0; i < notesArr.length; i++) {
+		let note = notes[i];
+		if(title == notes[i].title) {
+			console.log('=== Is not unique ===')
+			return false;
+		}
+	}
+	return true;
 };
 
 
