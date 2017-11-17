4c05b5c7bc81bd6829826d7b4f57695e9d0bf860
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-17 14:31:02 -0800

    fitching data from the API

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,8 +3,10 @@ console.log('Starting app')
 const request = require('request');
 
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%20san%20francisco',
+	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=23%20gaery%20street%20san%20francisco',
 	json: true
 }, (error, response, body) => {
-	console.log(body)
-})
+	console.log(`Adress: ${body.results[0].formatted_address}`);
+	console.log(`LAT: ${body.results[0].geometry.location.lat}`);
+	console.log(`LNG: ${body.results[0].geometry.location.lng}`);
+});
