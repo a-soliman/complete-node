dfe26636c6775db544a5d128d4918bfb3a5a8347
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-17 14:15:57 -0800

    installed request and made the first call to the API

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,1 +1,10 @@
 console.log('Starting app')
+
+const request = require('request');
+
+request({
+	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%20san%20francisco',
+	json: true
+}, (error, response, body) => {
+	console.log(body)
+})
