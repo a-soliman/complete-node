8c8bd2eced19c4f91c5d3bafcfefc60edc42facb
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-18 21:18:44 -0800

    more complex examples with promises

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,13 +1,35 @@
-let somePromise = new Promise((resolve, reject) => {
-	
-	setTimeout(()=> {
-		//resolve('Hey, It worked!');
-		reject('unable to fulfill promise');
-	}, 2500)
-});
+let asyncAdd = (a, b) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(()=> {
+			if( typeof a === 'number' && typeof b === 'number') {
+				resolve(a + b);
+			}
+			else {
+				reject('Arguments must be numbers');
+			}
 
-somePromise.then((data)=> {
-	console.log(data);
-}).catch((message) => {
-	console.log(message)
-})
+		}, 1500);
+	});
+}
+
+asyncAdd(5, 'f')
+	.then((data)=> {
+		console.log(data);
+	})
+	.catch((message) => {
+		console.log('Error: ', message)
+	}
+);
+// let somePromise = new Promise((resolve, reject) => {
+	
+// 	setTimeout(()=> {
+// 		//resolve('Hey, It worked!');
+// 		reject('unable to fulfill promise');
+// 	}, 2500)
+// });
+
+// somePromise.then((data)=> {
+// 	console.log(data);
+// }).catch((message) => {
+// 	console.log(message)
+// })
