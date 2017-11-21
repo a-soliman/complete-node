556e489799f1ef314655914651346b0d50d6d346
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-20 21:09:03 -0800

    working with hbs partials

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,33 +1,70 @@
 const express 	= require('express');
-const hbs		= require('hbs')
-
+const hbs		= require('hbs');
 
 let app = express();
 
+
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-	//res.send('<h1>Hello Express!</h1>');
-	res.send({
-		name: 'Ahmed',
-		likes: [
-		'Biking',
-		'resting'
-		]
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		currentYear: new Date().getFullYear(),
+		message: 'Welcome to home!'
 	})
 });
 
-app.get('/about', (req,res) => {
-	res.send('Welcome to about page')
-});
-
-app.get('/bad', (re1, res) => {
-	res.send({
-		Error: 'Error has occured!'
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+		currentYear: new Date().getFullYear()
 	})
 })
 
-app.listen(3000, ()=> {
-	console.log('Server is up on port 3000')
-});
+
+app.get('/home', (req, res)=> {
+	console.log('requested home');
+	res.sernd('Hello from home!')
+})
+
+
+app.listen(3001)
+
+// app.listen('port', () => {
+// 	console.log('server is runing on port 3000')
+// });
+// const express 	= require('express');
+// const hbs		= require('hbs')
+
+
+// let app = express();
+
+// app.set('view engine', 'hbs');
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', (req, res) => {
+// 	//res.send('<h1>Hello Express!</h1>');
+// 	res.send({
+// 		name: 'Ahmed',
+// 		likes: [
+// 		'Biking',
+// 		'resting'
+// 		]
+// 	})
+// });
+
+// app.get('/about', (req,res) => {
+// 	res.render('about.hbs');
+// });
+
+// app.get('/bad', (re1, res) => {
+// 	res.send({
+// 		Error: 'Error has occured!'
+// 	})
+// })
+
+// app.listen(3000, ()=> {
+// 	console.log('Server is up on port 3000')
+// });
