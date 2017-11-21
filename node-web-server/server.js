7736f347474fe36f613a25f051b9542fd059a4ba
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-20 21:38:54 -0800

    working with middleware adding a logger file

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,5 +1,6 @@
 const express 	= require('express');
 const hbs		= require('hbs');
+const fs		= require('fs');
 
 let app = express();
 
@@ -7,6 +8,20 @@ let app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+
+app.use((req, res, next) => {
+	let now = new Date().toString();
+
+	let log = `${now}: ${req.method} : ${req.url}`;
+	fs.appendFile('server.log', log + '\n', (err) =>  {
+		if(err) {
+			console.log('Unable to append to server.log!')
+		}
+	});
+
+	next();
+});
 
 hbs.registerHelper('getCurrentYear', () => {
 	return new Date().getFullYear()
