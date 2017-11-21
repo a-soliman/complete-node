d950204c0cad1dadc0b320af1e98388221f2a2ed
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-21 13:04:48 -0800

    setup start secript and heroku port

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,6 +2,7 @@ const express 	= require('express');
 const hbs		= require('hbs');
 const fs		= require('fs');
 
+const port = process.env.PORT || 3000;
 let app = express();
 
 
@@ -57,41 +58,6 @@ app.get('/home', (req, res)=> {
 })
 
 
-app.listen(3001)
-
-// app.listen('port', () => {
-// 	console.log('server is runing on port 3000')
-// });
-// const express 	= require('express');
-// const hbs		= require('hbs')
-
-
-// let app = express();
-
-// app.set('view engine', 'hbs');
-// app.use(express.static(__dirname + '/public'));
-
-// app.get('/', (req, res) => {
-// 	//res.send('<h1>Hello Express!</h1>');
-// 	res.send({
-// 		name: 'Ahmed',
-// 		likes: [
-// 		'Biking',
-// 		'resting'
-// 		]
-// 	})
-// });
-
-// app.get('/about', (req,res) => {
-// 	res.render('about.hbs');
-// });
-
-// app.get('/bad', (re1, res) => {
-// 	res.send({
-// 		Error: 'Error has occured!'
-// 	})
-// })
-
-// app.listen(3000, ()=> {
-// 	console.log('Server is up on port 3000')
-// });
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`)
+})
