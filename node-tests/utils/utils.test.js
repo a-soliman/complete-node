fe1ed0b6500747879bb5c1c47e37a3ef9c0dcfb0
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-22 09:56:15 -0800

    more complex tests

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -15,3 +15,29 @@ it('Should square a number', () => {
 
 	expect(res).toBe(doubleNumber).toBeA('number');
 });
+
+it('Should verify first and last names are set', () => {
+	let user1 = {};
+	let fullName = 'Ahmed Soliman';
+	let testNames = fullName.split(' ');
+
+
+	let res = utils.setName(user1, fullName);
+
+	expect(res).toIncludeKeys(['firstName', 'lastName']);
+	expect(res.firstName).toEqual(testNames[0]).toBeA('string');
+
+	expect(res.lastName).toEqual(testNames[1]).toBeA('string');
+});
+
+
+// it('Should expect some value', () => {
+// 	// equal !== toBe
+// 	expect({name: 'Ahmed'}).toEqual({name: 'Ahmed'});
+
+// 	// to Include
+// 	expect([2,3,4]).toInclude(2);
+
+// 	// toExclude
+// 	expect([2,3,4]).toExclude(5)
+// });
