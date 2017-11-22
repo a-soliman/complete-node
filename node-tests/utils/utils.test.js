4b4b41b0e2c3f70da2ce260934cdb3e75608a729
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-22 09:36:06 -0800

    more with expect libraty

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -5,7 +5,7 @@ const utils = require('./utils');
 it('should add two numbers', () => {
 	let res = utils.add(10, 20);
 
-	expect(res).toBe(30);
+	expect(res).toBe(30).toBeA('number');
 });
 
 it('Should square a number', () => {
@@ -13,5 +13,5 @@ it('Should square a number', () => {
 	let doubleNumber = (number * number);
 	let res = utils.square(number);
 
-	expect(res).toBe(doubleNumber);
+	expect(res).toBe(doubleNumber).toBeA('number');
 });
