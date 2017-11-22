887529cb0a34de98ad76ffc328756e01cda82b7f
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-22 09:07:13 -0800

    coded a sample module and a samplen unit test function to test an add method

---
+++ node-tests/utils/utils.test.js
@@ -0,0 +1,9 @@
+const utils = require('utils');
+
+it('should add two numbers', () => {
+	let res = utils.add(10, 20);
+
+	if(res !== 30) {
+		throw new Error(`Expecting 30, but got ${res}`);
+	};
+});
