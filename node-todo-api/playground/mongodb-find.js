5c074658c5306d8b90a9aa8cb6952b4ac402cc1d
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-28 08:28:42 -0800

    passing query to find

--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -9,28 +9,14 @@ MongoClient.connect(dbURL, ( err, db ) => {
 	}
 	console.log('Succssefully Connected to DB');
 
-	db.collection('Todos').find().toArray().then((docs) => {
+	db.collection('Todos').find({completed: true}).toArray().then((docs) => {
 		console.log( JSON.stringify(docs, null, 2 ));
 	}, ( err ) => {
 		console.log('An error has occured!');
 	})
 
-	
+
 
 	db.close()
 
 });
-// MongoClient.connect(dbURL, (err, db) => {
-// 	if( err ) {
-// 		return console.log('Unable to connect to MongoDB server.');
-// 	}
-// 	console.log('Connected to MongoDB.');
-
-// 	db.collection('Todos').find().toArray().then((docs) => {
-// 		console.log(docs);
-// 	}, ( err ) => {
-// 		console.log('An error has occured ', err);
-// 	})
-
-// 	db.close();
-// });
