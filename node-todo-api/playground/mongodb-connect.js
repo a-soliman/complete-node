9bc44c09aa0d7a597a90a1737ee17efba239d843
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-28 08:04:01 -0800

    playground with mongo and mlab

---
+++ node-todo-api/playground/mongodb-connect.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const dbURL = 'mongodb://ahmed_soliman:abc123@ds123556.mlab.com:23556/node-todo-api';
+
+MongoClient.connect(dbURL, (err, db) => {
+	if( err ) {
+		return console.log('Unable to connect to MongoDB server.');
+	}
+	console.log('Connected to MongoDB.');
+
+	db.collection('Todos').insertOne({ 
+		text: 'Something to do.',
+		completed: false
+		}, (err, result) => {
+			if( err ) {
+				return console.log('Unable to insert todo ', err);
+			}
+
+			console.log(JSON.stringify(result.ops, null, 2))
+		})
+	db.close();
+});
