6f31ac92f1f5c8f05f1b38ba3d35967dfa801918
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-29 15:05:11 -0800

    specified the native promise library and initialized the connection

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise
+
+const dbURL = 'mongodb://ahmed_soliman:abc123@ds123556.mlab.com:23556/node-todo-api';
+
+
+mongoose.connect(dbURL);
+
 
 
 
