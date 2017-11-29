4956da2c7d7364e7450f95f6bc5779e5692271db
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-29 15:05:44 -0800

    coded the first mongoose model

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -8,5 +8,19 @@ const dbURL = 'mongodb://ahmed_soliman:abc123@ds123556.mlab.com:23556/node-todo-
 mongoose.connect(dbURL);
 
 
+// creating mongoose-model
+const Todo = mongoose.model('Todo', {
+	text: {
+		type: String
+	},
+	completed: {
+		type: Boolean
+	},
+	completedAt: {
+		type: Number
+	}
+});
+
+
 
 
