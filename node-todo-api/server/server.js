7dea46f5a31b28c9d87c7939e0dc454d8230da56
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-30 07:40:15 -0800

    more with schemas

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -11,7 +11,8 @@ mongoose.connect(dbURL);
 // creating mongoose-model
 const Todo = mongoose.model('Todo', {
 	text: {
-		type: String
+		type: String,
+		required: true
 	},
 	completed: {
 		type: Boolean
@@ -23,16 +24,23 @@ const Todo = mongoose.model('Todo', {
 
 // creaing a todo
 
-let newTodo = new Todo({
-	text: 'Study chapter 11',
-	completed: true,
-	completedAt: 27
-});
+// let newTodo = new Todo({
+// 	text: 'Study chapter 11',
+// 	completed: true,
+// 	completedAt: 27
+// });
 
-newTodo.save().then((doc) => {
+// newTodo.save().then((doc) => {
+// 	console.log(JSON.stringify(doc, null, 2))
+// }, (err) => {
+// 	console.log('unable to save newTodo ', err);
+// })
+
+
+let otherTodo = new Todo({})
+
+otherTodo.save().then((doc) => {
 	console.log(JSON.stringify(doc, null, 2))
 }, (err) => {
-	console.log('unable to save newTodo ', err);
+	console.log('an error ... ', err);
 })
-
-
