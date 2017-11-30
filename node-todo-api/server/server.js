dec2a151672d9ef3ef74b6b5d698df0cc0e69c87
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-30 11:16:09 -0800

    setp the first post route for a todo, tested it through postman

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -9,7 +9,22 @@ const port = process.env.PORT || 3000;
 
 let app = express();
 
+app.use(bodyParser.json());
+
+
+// POST
+app.post('/todos', (req, res) => {
+	let todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc) => {
+		res.send({ status: 'Saved', doc: doc });
+	}, (err) => {
+		res.status(400).send(err)
+	})
+});
 
 app.listen(port, () => {
-	console.log('Server is on at ' + port);
-})
+	console.log(`Server is on at ${port}.`);
+});
