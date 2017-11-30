b8da12a7268c6b9de0ee0653b9a2f583a21e2295
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-30 12:26:28 -0800

    added 4 test cases 2 for post todos and 2 for post users

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -3,9 +3,14 @@ const request 	= require('supertest');
 
 const { app }	= require('./../server');
 const { Todo }  = require('./../models/todo');
+const { User }  = require('./../models/user');
 
 beforeEach((done) => {
 	Todo.remove({}).then(() => { done(); });
+});
+
+beforeEach((done) => {
+	User.remove({}).then(() => { done(); });
 });
 
 
@@ -32,4 +37,64 @@ describe('POST /todos', () => {
 				}).catch(e => done(e));
 			});
 	});
+
+	it('Should Not create todo with invalid body data', (done) => {
+
+		request(app)
+			.post('/todos')
+			.send({})
+			.expect(400)
+			.end(( err, res) => {
+				if( err ) {
+					return done(err)
+				}
+
+				Todo.find().then((todos) => {
+					expect(todos.length).toBe(0);
+					done();
+				}).catch(e => done(e));
+			})
+	})
+})
+
+describe('POST /users', () => {
+	it('Should create a new user', (done) => {
+		let email = 'test@user.com'
+
+		request(app)
+		.post('/users')
+		.send({ email })
+		.expect(200)
+		.expect(( res ) => {
+			expect(res.body.doc.email).toBe(email);
+		})
+		.end(( err, res ) => {
+			if ( err ) {
+				return done(err);
+			}
+
+			User.find().then((users) => {
+				expect(users.length).toBe(1);
+				expect(users[0].email).toBe(email);
+				done();
+			}).catch(e => done(e));
+		});
+	});
+	it('Should not create a new user with bad data', (done) => {
+
+		request(app)
+			.post('/users')
+			.send({})
+			.expect(400)
+			.end(( err, res) => {
+				if ( err ) { 
+					return done(err)
+				}
+
+				User.find().then((users) => {
+					expect(users.length).toBe(0);
+					done()
+				}).catch(e => done(e));
+			})
+	})
 });
